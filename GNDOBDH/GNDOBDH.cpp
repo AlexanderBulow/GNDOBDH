0c184e2be33ce3e4fc95c910496e572aadccdc6f
AlexanderBulow <alexandercarlaxelbulow@gmail.com>
2024-10-16 17:27:05 +0200

    Add project files.

---
+++ GNDOBDH/GNDOBDH.cpp
@@ -0,0 +1,19 @@
+﻿// GNDOBDH.cpp : Defines the entry point for the application.
+//
+
+#include "GNDOBDH.h"
+#include "SendCOmmand.h"
+#include "GetCommand.h"
+using namespace std;
+
+int main()
+{
+	while (true) {
+		char command[64] = "something";
+		SendCom(command, "GNDtoPF");
+
+		char receiving[32];
+		GetCommand(receiving, "GNDtoPF");
+	}
+	return 0;
+}
