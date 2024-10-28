b5553668b59aa732c489875b47d6aeadb483c7f6
AlexanderBulow <alexandercarlaxelbulow@gmail.com>
2024-10-28 22:06:03 +0100

    added more ports for better communication

--- GNDOBDH/GNDOBDH.cpp
+++ GNDOBDH/GNDOBDH.cpp
@@ -21,21 +21,23 @@ int main()
 		cout << TC << endl;
 		if (TC[0] == '\0') {
 			TC = "X";
+			cout << "test";
 			SendCom(TC, 1);
 		}
 		else {
 			SendCom(TC, 1);
 		}
 		//gets acknowlegement and prints it 
-		char* receiving = GetCommand(1);
+		char* receiving = GetCommand(2);
 		cout << receiving << endl;
 
 		//random data
-		char* receiving2 = GetCommand(1);
+		char* receiving2 = GetCommand(3);
 		printTM_and_log(receiving2);
 		//housekeeping
-		char* receiving3 = GetCommand(1);
+		char* receiving3 = GetCommand(4);
 		printTM_and_log(receiving3);
+		sleep(1);
 	}
 	return 0;
 }
